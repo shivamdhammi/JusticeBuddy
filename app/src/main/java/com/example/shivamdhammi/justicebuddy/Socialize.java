0a5b895e3c9056e3fb36aaf44961ea9d9b5ebfff
Yahska8740 <41650660+Yahska8740@users.noreply.github.com>
2018-10-21 00:34:34 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/shivamdhammi/justicebuddy/Socialize.java
@@ -0,0 +1,53 @@
+package com.example.shivamdhammi.justicebuddy.Fragmentstab;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.example.shivamdhammi.justicebuddy.R;
+import com.example.shivamdhammi.justicebuddy.uploadSocialize;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Socialize extends Fragment {
+
+    //uploasing
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.socialize, container, false);
+
+        return view;
+
+
+    }
+
+
+    public List<uploadSocialize> setdata(String imageuril){
+        List<uploadSocialize> mock = new ArrayList<>();
+        uploadSocialize test = new uploadSocialize();
+
+
+
+        mock.add(test);
+        return mock;
+    }
+}
---
+++ app/src/main/java/com/example/shivamdhammi/justicebuddy/crowd_funding.java
@@ -0,0 +1,24 @@
+package com.example.shivamdhammi.justicebuddy.Fragmentstab;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shivamdhammi.justicebuddy.R;
+
+
+public class crowd_funding extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crowd_funding, container, false);
+
+        return view;
+
+
+    }
+}
